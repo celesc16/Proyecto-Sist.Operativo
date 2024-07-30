6d9d26af3bbbcb68d0aa78ca613b8de6bfdcd37b
Relyckon <relyckon4188@gmail.com>
2024-07-30 15:57:40 -0400

    customer.py-->client.py    modified:client.py

--- client.py
+++ client.py
@@ -1,21 +1,21 @@
 import os
 
-class Customer:
+class Client:
     def __init__(self, fifo_path):
         self.fifo_path = fifo_path
 
     def send_messaje(self, messaje):
         
         if not os.path.exists(self.fifo_path):
-            raise FileNotFoundError(f"The FIFO was not found in the path: {self.fifo_path}")
+            raise FileNotFoundError(f"El FIFO no se ha encontrado en la ruta: {self.fifo_path}")
         
         with open(self.fifo_path, 'w') as fifo:
             fifo.write(messaje)
-            print(f"Customer sended: {messaje}")
+            print(f"Cliente envio: {messaje}")
 
 if __name__ == "__main__":
     fifo_path = "/tmp/fifo_server"
-    customer = Customer(fifo_path)
+    client = Client(fifo_path)
 
     messaje = "ricuet"
-    customer.send_messaje(messaje)
+    client.send_messaje(messaje)

e5dc8af029292bfb6c0c72677515f38083df9824
Relyckon <relyckon4188@gmail.com>
2024-08-05 08:28:09 -0400

    se aniadieron cambio

--- client.py
+++ client.py
@@ -1,28 +1,29 @@
 import os
 
 class Client:
-    def __init__(self, fifo_path):
+    def __init__(self, fifo_path, response_fifo_path): 
         self.fifo_path = fifo_path
         self.response_fifo_path = response_fifo_path
 
-    def send_messaje(self, messaje):
+    def send_message(self, message): 
         if not os.path.exists(self.fifo_path):
             raise FileNotFoundError(f"El FIFO no se ha encontrado en la ruta: {self.fifo_path}")
-        
+
         with open(self.fifo_path, 'w') as fifo:
-            fifo.write(messaje)
-            print(f"Cliente envio: {messaje}")
+            fifo.write(message)
+            print(f"Cliente envió: {message}")
 
     def receive_response(self):
         with open(self.response_fifo_path, 'r') as fifo:
             response = fifo.read().strip()
-            print(f"Client received: {response}")
+            print(f"Cliente recibió: {response}")
             return response
 
 if __name__ == "__main__":
     fifo_path = "/tmp/fifo_server"
+    response_fifo_path = "/tmp/fifo_response"
     client = Client(fifo_path, response_fifo_path)
 
-    messaje = "ricuet"
-    client.send_messaje(messaje)
+    message = "ricuet"
+    client.send_message(message)  
     response = client.receive_response()
--- server.py
+++ server.py
@@ -1,10 +1,9 @@
-
-import os 
-import time
+import os
 
 class Server:
     def __init__(self):
         self.fifo_path = "/tmp/fifo_server"
+        self.response_fifo_path = "/tmp/fifo_response"
         self.create_fifo()
 
     def create_fifo(self):
@@ -14,27 +13,33 @@ class Server:
         else:
             print(f"fifo {self.fifo_path} ya existe")
 
+        if not os.path.exists(self.response_fifo_path): 
+            os.mkfifo(self.response_fifo_path)
+            print(f"fifo {self.response_fifo_path} creada")
+        else:
+            print(f"fifo {self.response_fifo_path} ya existe")
+
     def listen(self):
         print("Servidor esperando mensaje...")
         with open(self.fifo_path, 'r') as fifo:
             while True:
                 data = fifo.read().strip()
                 if data:
-                    print(f"Servidor recibio: data")
-                    response = self.process_request(data)
+                    print(f"Servidor recibió: {data}")
+                    response = self.process_request(data)  
                     self.respond_to_client(response)
 
-    def procces_request(self,response):
+    def process_request(self, request):  
         if request.lower() == "ricuet":
-            return "Hola! Request recibido!. Como puedo ayudarte?"
+            return "Hola! Request recibido!. ¿Cómo puedo ayudarte?"
         else:
-            return " Lo siento!, no se puedo procesar tu solicitud"
+            return "Lo siento, no se pudo procesar tu solicitud."
 
-    def respond_to_client(self,response):
-        with open(self.fifo_path, 'w' ) as fifo:
+    def respond_to_client(self, response):
+        with open(self.response_fifo_path, 'w') as fifo: 
             fifo.write(response)
-            print(f"Respuesta enviada al cliente : {response}")
+            print(f"Respuesta enviada al cliente: {response}")
 
 if __name__ == "__main__":
-        server = Server()
-        server.listen() 
+    server = Server()
+    server.listen()
